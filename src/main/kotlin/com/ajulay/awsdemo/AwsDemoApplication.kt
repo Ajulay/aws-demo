1b35c5a760ab676923b6f4de7922ed4fac76b8fc
ajulay <11v09v@gmail.com>
2024-05-15 18:56:33 -0600

    first commit

---
+++ src/main/kotlin/com/ajulay/awsdemo/AwsDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.ajulay.awsdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AwsDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<AwsDemoApplication>(*args)
+}
---
+++ src/main/kotlin/com/ajulay/awsdemo/Employee.kt
@@ -0,0 +1,24 @@
+package com.ajulay.awsdemo
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import java.time.LocalDate
+import java.util.UUID
+
+
+@Entity
+class Employee(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    val id: Long,
+
+    @Column
+    val name: String,
+
+    @Column
+    val age: LocalDate,
+)
